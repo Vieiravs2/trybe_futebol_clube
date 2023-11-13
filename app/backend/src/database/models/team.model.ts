006adc21a6f9f0e757f2122b072492565a6253b9
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-11-13 19:11:10 -0300

    Feat: Criação da Model e Migration Teams

---
+++ app/backend/src/database/models/team.model.ts
@@ -0,0 +1,24 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.'
+
+export default class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
