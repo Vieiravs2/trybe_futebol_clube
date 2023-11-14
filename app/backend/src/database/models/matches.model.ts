e08b050086d6014d627685795b6f380e1eb610e4
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-11-14 01:45:15 -0300

    Feat: Criação das Migrations e Models Matches e Users

---
+++ app/backend/src/database/models/matches.model.ts
@@ -0,0 +1,45 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+export default class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
---
+++ app/backend/src/database/models/users.model.ts
@@ -0,0 +1,40 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+export default class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  underscored: true,
+  timestamps: false,
+});
